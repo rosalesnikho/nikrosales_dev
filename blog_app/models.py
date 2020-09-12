98cd57a52350502483d778131801f822b398882f
rosalesnikhjo <rosalesnikho@gmail.com>
2020-09-12 15:58:22 -0500

    added MVC for blog section

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Blog Post Model.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Allows Modeled Data to show in Python Admin Panel
+    def __str__(self):
+        title = self.title
+        return title
